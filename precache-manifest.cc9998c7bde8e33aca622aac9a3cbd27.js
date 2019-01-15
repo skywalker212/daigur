d362797b3262b7d02cf43721a4f74dd065d49771
Akash Gajjar <akashgajjar8@gmail.com>
2019-01-15 14:08:58 +0530

    Updates

---
+++ precache-manifest.cc9998c7bde8e33aca622aac9a3cbd27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d92046cb40d0a0d6da2",
+    "url": "/daigur/static/js/runtime~main.9d92046c.js"
+  },
+  {
+    "revision": "291269d843b39d9ecf70",
+    "url": "/daigur/static/js/main.291269d8.chunk.js"
+  },
+  {
+    "revision": "f31dc12f8da0d4880be6",
+    "url": "/daigur/static/js/1.f31dc12f.chunk.js"
+  },
+  {
+    "revision": "291269d843b39d9ecf70",
+    "url": "/daigur/static/css/main.1f65b296.chunk.css"
+  },
+  {
+    "revision": "e81c862e474f9d3f4a753d5e4116cb2a",
+    "url": "/daigur/index.html"
+  }
+];
